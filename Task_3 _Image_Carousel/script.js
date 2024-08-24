a936b324328636a71037bb53d3170bf3292efcf9
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-08-24 16:17:29 +0530

    code modified

---
+++ Task_3 _Image_Carousel/script.js
@@ -0,0 +1,10 @@
+let images = document.querySelectorAll(".images-container img");
+let currIndex = 0;
+
+function showNextImage() {
+  images[currIndex].classList.remove("active");
+  currIndex = (currIndex + 1) % images.length;
+  images[currIndex].classList.add("active");
+}
+
+setInterval(showNextImage, 3000);
